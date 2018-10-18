82a107d42437b9a3d6f57fd81fc27e75defe6712
Amanda Bardeau <amanda.bardeau@gmail.com>
2018-10-18 09:29:34 -0500

    first commit

---
+++ Lab8 Script.R
@@ -0,0 +1,23 @@
+snpcol[gwastab$Chromosome == chrnames[i]] <- allcol[i]
+}
+
+# draw plot
+par(mar = c(3.1, 3.1, 1.1, 1.1), mgp = c(2, 0.5, 0))
+plot(transp, col = snpcol, pch = 16, axes = FALSE,
+     xlab = "Genomic location", ylab = "-log10(p)")
+abline(h = threshold.lines, lty = 2, col = "darkgrey")
+axis(2) # add in standard y-axis
+
+# label points above the cutoff
+tolabel <- which(transp >= label.cutoff)
+text(tolabel, transp[tolabel], gwastab$SNP[tolabel])
+
+# axis with chromosome labels
+ticklocs <- numeric(nchr) # set up empty vector
+for(i in 1:nchr){
+  ticklocs[i] <- median(which(gwastab$Chromosome == chrnames[i]))
+}
+axis(1, at = ticklocs, labels = as.character(chrnames))
+}
+# fancy version
+manhattan3(yieldGWAS, threshold.lines = c(5, 8), label.cutoff = 5)
